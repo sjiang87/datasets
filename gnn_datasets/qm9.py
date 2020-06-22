4087aff7614d5ae1b5463e75f8f7686b732c5722
sjiang87 <sjiang87@wisc.edu>
2020-06-21 19:25:42 -0500

    qm9 dataset

--- gnn_datasets/qm9.py
+++ gnn_datasets/qm9.py
@@ -1,6 +1,8 @@
 """
 Author: Shengli Jiang
 Email: sjiang87@wisc.edu / shengli.jiang@anl.gov
+Adapted from Spektral tf1
+https://github.com/danielegrattarola/spektral/tree/tf1
 """
 
 import os
@@ -8,6 +10,8 @@ from tensorflow.keras.utils import get_file
 
 DATA_PATH = os.path.expanduser('~/.deephyper/datasets/qm9/')
 DATASET_URL = 'http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/gdb9.tar.gz'
+NODE_FEATURES = ['atomic_num', 'charge', 'coords', 'iso']
+EDGE_FEATURES = ['type', 'stereo']
 
 
 def _download_data():
@@ -23,6 +27,35 @@ def _download_data():
     os.rename(DATA_PATH + 'gdb9.sdf', DATA_PATH + 'qm9.sdf')
     os.rename(DATA_PATH + 'gdb9.sdf.csv', DATA_PATH + 'qm9.sdf.csv')
     os.remove(DATA_PATH + 'qm9.tar.gz')
+
+
+def load_data(nf_keys=None, ef_keys=None):
+    """
+    Loads the QM9 chemical data set of small molecules.
+
+    Nodes represent heavy atoms (hydrogens are discarded), edges represent
+    chemical bonds.
+
+    The node features represent the chemical properties of each atom, and are
+    loaded according to the `nf_keys` argument.
+    See `deephyper.datasets.gnndataset.qm9.NODE_FEATURES` for possible node features, and
+    see [this link](http://www.nonlinear.com/progenesis/sdf-studio/v0.9/faq/sdf-file-format-guidance.aspx)
+    for the meaning of each property. Usually, it is sufficient to load the
+    atomic number.
+
+    The edge features represent the type and stereoscopy of each chemical bond
+    between two atoms.
+    See `deephyper.datasets.genn_datasets.qm9.EDGE_FEATURES` for possible edge features, and
+    see [this link](http://www.nonlinear.com/progenesis/sdf-studio/v0.9/faq/sdf-file-format-guidance.aspx)
+    for the meaning of each property. Usually, it is sufficient to load the
+    type of bond.
+    Args:
+        nf_keys: list or str, node node features to return (see `qm9.NODE_FEATURES` for available features);
+        ef_keys: list or str, edge features to return (see `qm9.EDGE_FEATURES` for available features);
+
+    Returns:
+        the adjacency matrix, node features, edge features, and a Pandas dataframe containing labels;
+    """
 
 
 def test_download_data():
