1824456894c0bc76056eb4492e25d65b4a5e7f2a
sjiang87 <sjiang87@wisc.edu>
2020-06-21 20:04:18 -0500

    qm9 dataset

--- gnn_datasets/qm9.py
+++ gnn_datasets/qm9.py
@@ -7,6 +7,9 @@ https://github.com/danielegrattarola/spektral/tree/tf1
 
 import os
 from tensorflow.keras.utils import get_file
+from spektral.utils.io import load_csv, load_sdf
+from spektral.chem import sdf_to_nx
+from spektral.utils import nx_to_numpy
 
 DATA_PATH = os.path.expanduser('~/.deephyper/datasets/qm9/')
 DATASET_URL = 'http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/gdb9.tar.gz'
@@ -29,7 +32,7 @@ def _download_data():
     os.remove(DATA_PATH + 'qm9.tar.gz')
 
 
-def load_data(nf_keys=None, ef_keys=None):
+def load_data(nf_keys=None, ef_keys=None, amount=None):
     """
     Loads the QM9 chemical data set of small molecules.
 
@@ -52,10 +55,40 @@ def load_data(nf_keys=None, ef_keys=None):
     Args:
         nf_keys: list or str, node node features to return (see `qm9.NODE_FEATURES` for available features);
         ef_keys: list or str, edge features to return (see `qm9.EDGE_FEATURES` for available features);
+        amount: the amount of molecules to return (in ascending order by number of atoms);
 
     Returns:
         the adjacency matrix, node features, edge features, and a Pandas dataframe containing labels;
     """
+    if not os.path.exists(DATA_PATH):
+        _download_data()    # Try to download dataset
+    print('Loading QM9 dataset.')
+
+    sdf_file = os.path.join(DATA_PATH, 'qm9.sdf')
+    data = load_sdf(sdf_file, amount=amount)  # Internal SDF format
+
+    # Load labels
+    labels_file = os.path.join(DATA_PATH, 'qm9.sdf.csv')
+    labels = load_csv(labels_file)
+    if amount is not None:
+        labels = labels[:amount]
+    # Convert to Networkx
+    data = [sdf_to_nx(_) for _ in data]
+    if nf_keys is not None:
+        if isinstance(nf_keys, str):
+            nf_keys = [nf_keys]
+    else:
+        nf_keys = NODE_FEATURES
+    if ef_keys is not None:
+        if isinstance(ef_keys, str):
+            ef_keys = [ef_keys]
+    else:
+        ef_keys = EDGE_FEATURES
+
+    adj, nf, ef = nx_to_numpy(data,
+                              auto_pad=True, self_loops=True,
+                              nf_keys=nf_keys, ef_keys=ef_keys)
+    return adj, nf, ef, labels
 
 
 def test_download_data():
@@ -63,4 +96,6 @@ def test_download_data():
 
 
 if __name__ == "__main__":
-    test_download_data()
+    adj, nf, ef, labels = load_data(nf_keys=['atomic_num', 'charge', 'coords', 'iso'],
+                                    ef_keys=['type', 'stereo'],
+                                    amount=1000)
